d5e90eb5f0f0a1825b5813148bcb0dbf3438194d
bekward <bekward@gmail.com>
2020-10-31 18:26:21 +0400

    link logo to main domain

--- resources/views/web/component/header.blade.php
+++ resources/views/web/component/header.blade.php
@@ -6,7 +6,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="index.html">Andia - a super cool design agency...</a>
+        <a class="navbar-brand" href="/`1">Andia - a super cool design agency...</a>
     </div>
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="top-navbar-1">
