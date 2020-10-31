beb21525ff52aaa84d24db2ba97b472da3358113
anofeles <kaxam1@gmail.com>
2020-10-31 07:03:51 -0700

    repositor creater

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -5,8 +5,19 @@ namespace App\Http\Controllers\Frontend;
 
 
 use App\Http\Controllers\Controller;
+use App\Repositories\MenuRepository;
 
 class FrontendController extends Controller
 {
+    protected $MenuRepository;
+    public function __construct(
+        MenuRepository $MenuRepository
+    )
+    {
+        $this->MenuRepository = $MenuRepository;
+    }
 
+    public function menuAll(){
+        return $this->MenuRepository->all();
+    }
 }
