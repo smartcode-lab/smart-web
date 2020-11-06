508c9fa03b4c407ff6c370eb7b61d6104ab61f31
achi777 <Didafxodi1!>
2020-11-06 21:58:43 +0400

    admin categories

--- resources/views/admin/web/component/left.blade.php
+++ resources/views/admin/web/component/left.blade.php
@@ -29,7 +29,7 @@
         </div>
         <div class="sb-sidenav-footer">
             <div class="small">Logged in as:</div>
-            Start Bootstrap
+            Bleno
         </div>
     </nav>
 </div>
